a9984280bd6c337840106b05302ba128d70e56b2
saeeddhqan <saeeddhqan@gmail.com>
2023-09-04 14:03:11 +0330

    up

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -19,10 +19,10 @@ batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 2
+n_layer = 4
 n_head = 6
 n_embd = 384
-dropout = 0.2
+dropout = 0.35
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
 max_iters = 5000
