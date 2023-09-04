bbae3e99524def90c314ee3d0d3c9810efa06da2
saeeddhqan <saeeddhqan@gmail.com>
2023-09-04 12:31:09 +0330

    up

--- model.py
+++ model.py
@@ -124,10 +124,10 @@ class GPT(nn.Module):
         self.config = config
         self.pad = 15
         self.extra_emb = ((self.pad * 5) + (self.pad * 4))
-
+        r = config.n_embd - self.extra_emb
         self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd - self.extra_emb),
-            wpe = nn.Embedding(config.block_size, config.n_embd - self.extra_emb),
+            wte = nn.Embedding(config.vocab_size, r),
+            wpe = nn.Embedding(config.block_size, r),
             eps_embs = nn.Embedding(config.vocab_size + 1, 5),
             eps_pos_embs = nn.Embedding(config.block_size, 4),
             drop = nn.Dropout(config.dropout),
@@ -139,7 +139,7 @@ class GPT(nn.Module):
         # "UserWarning: functional_call was passed multiple values for tied weights.
         # This behavior is deprecated and will be an error in future versions"
         # not 100% sure what this is, so far seems to be harmless. TODO investigate
-        self.transformer.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying
+        # self.transformer.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying
 
         # init all weights
         self.apply(self._init_weights)
