ac21da833b31fb8d862b0b847e8f628fc7b03523
saeeddhqan <saeeddhqan@gmail.com>
2023-09-05 00:30:42 +0330

    grad

--- model.py
+++ model.py
@@ -127,13 +127,14 @@ class GPT(nn.Module):
         self.config = config
         self.pad = 4
         self.extra_emb = ((self.pad * 4) + (self.pad * 4))
-        r = config.n_embd - self.extra_emb
+        # r = config.n_embd - self.extra_emb
+        r = config.n_embd
         self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.vocab_size, r),
             wpe = nn.Embedding(config.block_size, r),
-            eps_embs = nn.Embedding(config.vocab_size + 1, 4),
-            eps_pos_embs = nn.Embedding(config.block_size, 4),
-            eps_drop = nn.Dropout(config.dropout),
+            # eps_embs = nn.Embedding(config.vocab_size + 1, 4),
+            # eps_pos_embs = nn.Embedding(config.block_size, 4),
+            # eps_drop = nn.Dropout(config.dropout),
             drop = nn.Dropout(config.dropout),
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
             ln_f = LayerNorm(config.n_embd, bias=config.bias),
@@ -185,19 +186,19 @@ class GPT(nn.Module):
         tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd)
         pos_emb = self.transformer.wpe(pos) # position embeddings of shape (t, n_embd)
 
-        xseq = F.pad(idx + 1, (self.pad, 0)).unfold(1, self.pad, 1)[:,:-1,:]
-        bseq = F.pad(pos, (self.pad, 0)).unfold(0, self.pad, 1)[1:,:]
-        eps_embs = self.transformer.eps_embs(xseq)
-        eps_pos_embs = self.transformer.eps_pos_embs(bseq)
-        eps_comb = torch.cat([
-            eps_embs.view(b, t, -1),
-            eps_pos_embs.view(1, t, -1).expand(b, t, -1)],
-            dim=-1,
-        )
-        eps_comb = self.transformer.eps_drop(eps_comb)
-        x = torch.cat([tok_emb + pos_emb, eps_comb], dim=-1)
+        # xseq = F.pad(idx + 1, (self.pad, 0)).unfold(1, self.pad, 1)[:,:-1,:]
+        # bseq = F.pad(pos, (self.pad, 0)).unfold(0, self.pad, 1)[1:,:]
+        # eps_embs = self.transformer.eps_embs(xseq)
+        # eps_pos_embs = self.transformer.eps_pos_embs(bseq)
+        # eps_comb = torch.cat([
+        #     eps_embs.view(b, t, -1),
+        #     eps_pos_embs.view(1, t, -1).expand(b, t, -1)],
+        #     dim=-1,
+        # )
+        # eps_comb = self.transformer.eps_drop(eps_comb)
+        # x = torch.cat([tok_emb + pos_emb, eps_comb], dim=-1)
 
-        # x = tok_emb + pos_emb
+        x = tok_emb + pos_emb
 
         x = self.transformer.drop(x)
         for block in self.transformer.h:
